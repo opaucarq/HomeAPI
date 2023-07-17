32d6c008c8cdca247118feb131520e1ba4ec036f
Oliver <opaucarq@uni.pe>
2023-07-17 17:32:13 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get post patch delete
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
 end
